d5470231f441c819113f81b6a77c572e237ad444
Vinicius <beneditoloura@gmail.com>
2021-09-21 19:56:20 -0300

    ✨ feat: create home and SubscribeButton component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/global.scss';
-import { Header } from './components/Header';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
