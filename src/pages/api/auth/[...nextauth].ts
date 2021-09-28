6cef588ccee7fd41a3b65759228faf92edaeb94e
Vinicius <beneditoloura@gmail.com>
2021-09-27 21:41:33 -0300

    ✨ feat: save user data on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -10,4 +13,15 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const { email } = user;
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch (error) {
+        return error;
+      }
+    },
+  },
 });
