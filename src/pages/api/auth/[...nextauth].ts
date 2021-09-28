1749e86414214f7cd2e95098bd2899c23a2e1ff6
Vinicius <beneditoloura@gmail.com>
2021-09-27 21:51:01 -0300

    ✨ feat: create query to verify user exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,7 +17,17 @@ export default NextAuth({
     async signIn(user, account, profile) {
       try {
         const { email } = user;
-        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+          )
+        );
         return true;
       } catch (error) {
         return error;
