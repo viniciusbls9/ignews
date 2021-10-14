b6ac52e241c3cb4b555db2d02e515875163a7bfe
Vinicius <beneditoloura@gmail.com>
2021-10-13 21:10:55 -0300

    ✨ feat: create service and use in post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -10,38 +25,48 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-              inventore officia esse assumenda dolorum quia dignissimos, at
-              voluptate quos repudiandae rerum consequuntur, sequi ipsa ex
-              perspiciatis pariatur similique impedit. Provident.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-              inventore officia esse assumenda dolorum quia dignissimos, at
-              voluptate quos repudiandae rerum consequuntur, sequi ipsa ex
-              perspiciatis pariatur similique impedit. Provident.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>titulo do post</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-              inventore officia esse assumenda dolorum quia dignissimos, at
-              voluptate quos repudiandae rerum consequuntur, sequi ipsa ex
-              perspiciatis pariatur similique impedit. Provident.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'publication')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'pharagraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
