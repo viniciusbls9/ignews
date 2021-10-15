c2c75a32c02e5b194e777f19ab80ac108160a1ce
Vinicius <beneditoloura@gmail.com>
2021-10-14 21:52:05 -0300

    ✨ feat: create preview post page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,7 +40,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
